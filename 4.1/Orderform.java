ce4541997174f6bcafb7a6b97d85ec7fc8dc32c0
770856 <770856@pdsb.net>
2024-12-07 13:08:00 -0500

    Update Orderform.java

--- 4.1/Orderform.java
+++ 4.1/Orderform.java
@@ -9,15 +9,24 @@ public class OrderForm implements ActionListener {
 
     public static void init() {
         JPanel panel = new JPanel();
-        JFrame frame = new JFrame("Welcome");
-        JLabel welcome = new JLabel("Welcome to the order menu!", SwingConstants.CENTER);
+        JFrame frame = new JFrame("Orderform");
+        
+        JLabel welcome = new JLabel("Welcome to the order menu!", SwingConstants.CENTER); // These are the text labels
         JLabel fries = new JLabel("Fries @ $1.50 each\n", SwingConstants.CENTER);
         JLabel burgers = new JLabel("Burgers @ $3.50 each\n", SwingConstants.CENTER);
         JLabel sodas = new JLabel("Soda's @ $1.50 each\n", SwingConstants.CENTER);
-        JTextField FText = new JTextField(SwingConstants.RIGHT);
+
+        JTextField FText = new JTextField(SwingConstants.RIGHT); // These 3 lines create the user input
         JTextField BText = new JTextField(SwingConstants.RIGHT);
         JTextField SText = new JTextField(SwingConstants.RIGHT);
-        JButton button = new JButton("Order Now!");
+
+        JButton button = new JButton("Order Now!"); // Order now button
+
+        JLabel statusBar = new JLabel("OrderForm running...", SwingConstants.LEFT); // Creates Status Bar
+        statusBar.setBorder(BorderFactory.createEtchedBorder());
+        statusBar.setFont(new Font("Arial", Font.PLAIN, 12));
+        statusBar.setBackground(Color.LIGHT_GRAY);
+
         panel.add(welcome);
         panel.add(fries);
         panel.add(FText);
@@ -26,9 +35,12 @@ public class OrderForm implements ActionListener {
         panel.add(sodas);
         panel.add(SText);
         panel.add(button);
+        panel.add(statusBar);
+
         frame.setSize(250,200); // Resize but using swing
         frame.add(panel);
-        frame.show();
+        frame.add(statusBar, BorderLayout.PAGE_END);
+        frame.setVisible(true);
 
     }
     public void actionPerformed (ActionEvent e) { } }
